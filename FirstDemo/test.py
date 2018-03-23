bc376802fc2f3fb9e9107ecd08d04c35b148f32f
fcu-d0449199 <wendy92433@gmail.com>
2018-03-23 20:44:11 +0800

    Update test.py

--- FirstDemo/test.py
+++ FirstDemo/test.py
@@ -2,11 +2,11 @@ import cv2
 from feature_matching import FeatureMatching
 
 #goal image
-img_train = cv2.imread('./9.jpg')
+img_train = cv2.imread('./2.jpg')
 
 #camera's image
-query_image = cv2.imread('./3.jpg')
-matching = FeatureMatching(query_image='./3.jpg')
+query_image = cv2.imread('./1.jpg')
+matching = FeatureMatching(query_image='./1.jpg')
 
 m, flag = matching.match(img_train, query_image)
 if(m == False): print("not match!!!")
