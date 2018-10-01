2d26b1069a8790046cb8c7a4e05460dc0f27674f
fcu-d0449199 <wendy92433@gmail.com>
2018-10-01 23:09:25 +0800

    Update Test_TakeOffAndLand.py

--- Test_TakeOffAndLand.py
+++ Test_TakeOffAndLand.py
@@ -22,8 +22,6 @@ if not connection_string:
 # Connect to the Vehicle
 print('Connecting to vehicle on: %s' % connection_string)
 print("(Connecting to vehicle on: /dev/ttyAMA0)")
-f.write("\n Connecting to vehicle on: %s" % connection_string)
-f.write("\n (Connecting to vehicle on: /dev/ttyAMA0)")
 vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
 # vehicle = connect(connection_string, wait_ready=True, baud=921600)
 
